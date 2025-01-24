267bdf125eeb7bd0d2d0c66d0d077311ae28fcc0
yeimyhs <helihuancasancho@gmail.com>
2025-01-23 22:52:45 -0500

    sesion toke

--- LlantasEcommerce/settings.py
+++ LlantasEcommerce/settings.py
@@ -67,9 +67,9 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    #'DEFAULT_PERMISSION_CLASSES': (
+    #    'rest_framework.permissions.IsAuthenticated',
+    #),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
     
