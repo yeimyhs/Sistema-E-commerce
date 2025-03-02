95805308338149f3da3165c743c3c5b74dc41cc9
yeimyhs <helihuancasancho@gmail.com>
2025-03-02 15:46:51 -0500

    lectura de excel para upload bulk

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1991,10 +1991,13 @@ class TblitemViewSet(ModelViewSet):
                 return Response({"error": "No se ha proporcionado ningún archivo."},
                                 status=status.HTTP_400_BAD_REQUEST)
 
+             # Leer archivo con UTF-8
             df = pd.read_excel(file, header=1, dtype=str, engine="openpyxl")
 
-# Normalizar caracteres especiales
+            # Convertir a UTF-8 y eliminar caracteres problemáticos
             df = df.applymap(lambda x: x.encode("utf-8", "ignore").decode("utf-8") if isinstance(x, str) else x)
+
+            # Remover tildes y caracteres especiales
             df = df.applymap(lambda x: unidecode.unidecode(x) if isinstance(x, str) else x)
 
             required_columns = ["CODIGO(SKU)", "NOMBRE DEL PRODUCTO", "STOCK", "PRECIO", "MARCA", "MODELO", "ESTADO"]
