63f1971117ea62c977ecc0f6b494ba9fd9baa02f
yeimyhs <helihuancasancho@gmail.com>
2025-03-05 22:11:45 -0500

    poblacion de bd y servicio token

--- LlantasEcommerce/urls.py
+++ LlantasEcommerce/urls.py
@@ -39,7 +39,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/ecommerce/swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    #path('api/ecommerce/swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/ecommerce/', include(('ecommerce.urls','ecommerce'))),
 ]
 
